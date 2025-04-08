dfda1c4f7fa71cb14e0b2504bd049e8f146d5420
Guilherme Campos <gh562061@gmail.com>
2025-04-08 13:09:44 -0300

    #

--- atvSete.ts
+++ atvSete.ts
@@ -1,7 +1,7 @@
 /*
 7- Faça um programa que pergunte qual número deseja calcular o fatorial, e exiba o resultado
 */
-
+//
 let teclado = require(`prompt-sync`)();
 let numero = parseInt(teclado("Digite o número que deseja calcular o fatorial: "));
 let fatorial = 1;
